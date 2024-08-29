db329a0ea14fdfb4635912a6231bdd2f97e5412e
JosephAaronMusara <josephaaronmusara@gmail.com>
2024-08-29 15:23:56 +0200

    Pagination script

---
+++ js/tablePagination.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+  $("#allTasksTable").DataTable();
+});
+$(document).ready(function () {
+  $("#taskListTable").DataTable();
+});
+$(document).ready(function () {
+  $("#assign-tasks").DataTable();
+});
+$(document).ready(function () {
+  $("#member-management").DataTable();
+});
+$(document).ready(function () {
+  $("#ranked-members").DataTable();
+});
